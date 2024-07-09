6e7b5874f900b8411e88a8a42f57dbced08a8f96
11D-Beyonder <ztz20001117@126.com>
2024-07-09 11:33:30 +0800

    feat[wezterm]: change color scheme to bamboo

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.color_scheme = "Monokai Soda"
+config.color_scheme = "Bamboo"
 config.font = wezterm.font("FantasqueSansM Nerd Font")
 config.font_size = 18
 
