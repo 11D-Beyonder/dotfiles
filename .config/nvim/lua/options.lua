7bb6595d69c981132a5c6ecc551b1954514613bf
11D-Beyonder <ztz20001117@126.com>
2024-07-09 10:39:15 +0800

    feat[nvim]: '<leader>y' to copy whole

--- .config/nvim/lua/options.lua
+++
@@ -1,2 +0,0 @@
-local opt = vim.o
-opt.background = "dark"
--- .config/nvim/lua/plugins/core/astrocore.lua
+++ .config/nvim/lua/plugins/core/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -32,6 +30,10 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        guifont = "FantasqueSansM Nerd Font",
+        tabstop = 4,
+        shiftwidth = 0,
+        cursorlineopt = "both",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -44,21 +46,21 @@ return {
     mappings = {
       -- first key is the mode
       n = {
+        ["<leader>y"] = {
+          function() vim.cmd "%y+" end,
+          desc = "File copy whole",
+        },
         -- second key is the lefthand side of the map
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
         -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
+        -- ["<Leader>bd"] = {
+        --   function()
+        --     require("astroui.status.heirline").buffer_picker(
+        --       function(bufnr) require("astrocore.buffer").close(bufnr) end
+        --     )
+        --   end,
+        --   desc = "Close buffer from tabline",
+        -- },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
