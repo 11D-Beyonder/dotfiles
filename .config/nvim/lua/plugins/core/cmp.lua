49243ad965c1d5ae104854d2fdd584fd88ad194c
11D-Beyonder <ztz20001117@126.com>
2024-07-09 13:13:41 +0800

    feat[nvim]: add cmp-cmdline.

---
+++ .config/nvim/lua/plugins/core/cmp.lua
@@ -0,0 +1,37 @@
+return { -- override nvim-cmp plugin
+  "hrsh7th/nvim-cmp",
+  keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+  dependencies = {
+    "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
+  },
+  opts = {
+    completion = {
+      completeopt = "menuone,noinsert,noselect",
+    },
+  },
+  config = function(_, opts)
+    local cmp = require "cmp"
+    cmp.setup(opts)
+
+    -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
+      }),
+    })
+  end,
+}
