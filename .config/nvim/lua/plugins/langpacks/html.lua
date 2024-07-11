5128d3f49be1ae00d6e8ffee9b83309849451fa2
11D-Beyonder <ztz20001117@126.com>
2024-07-12 00:39:25 +0800

    feat(nvim): add html&css support.

---
+++ .config/nvim/lua/plugins/langpacks/html.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "css" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "cssls", "emmet_ls" })
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    opts = {
+      config = {
+        html = { init_options = { provideFormatter = false } },
+        cssls = { init_options = { provideFormatter = false } },
+      },
+    },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "prettierd" })
+    end,
+  },
+}
