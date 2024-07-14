014d9362589284e50175ccd09cd9dd3ebbe92140
Tianze Zhu <ztz20001117@gmail.com>
2024-07-14 22:58:27 +0800

    refactor(nvim): copy https://github.com/chaozwn/astronvim_with_coc_or_mason/blob/self/lua/plugins/cmp.lua

--- .config/nvim/lua/plugins/core/cmp.lua
+++ .config/nvim/lua/plugins/core/cmp.lua
@@ -31,26 +31,58 @@ return {
       end,
     },
   },
-  opts = {
-    completion = {
-      completeopt = "menuone",
-    },
-    mapping = {
-      ["<C-K>"] = cmp.mapping(
-        function() cmp.select_prev_item { behavior = cmp.SelectBehavior.Select } end,
-        { "i", "c" }
-      ),
-      ["<C-J>"] = cmp.mapping(
-        function() cmp.select_next_item { behavior = cmp.SelectBehavior.Select } end,
-        { "i", "c" }
-      ),
-      ["<C-N>"] = cmp.mapping(function()
-        if luasnip.jumpable(1) then luasnip.jump(1) end
-      end, { "i", "c" }),
-      ["<C-P>"] = cmp.mapping(function()
-        if luasnip.jumpable(-1) then luasnip.jump(-1) end
-      end, { "i", "c" }),
-      ["<Tab>"] = cmp.config.mapping(cmp.mapping.complete(), { "i", "c" }),
-    },
-  },
+  opts = function(_, opts)
+    local function trim(s)
+      if s == nil then return "" end
+      return (s:gsub("^%s*(.-)%s*$", "%1"))
+    end
+    local function truncateString(s, maxLength)
+      if #s > maxLength then
+        return string.sub(s, 1, maxLength) .. "..."
+      else
+        return s
+      end
+    end
+    local function getMethodName(s) return string.gsub(s, "%(.*%)", "") end
+
+    return require("astrocore").extend_tbl(opts, {
+      completion = {
+        completeopt = "menuone",
+      },
+      formatting = {
+        fields = { "abbr", "menu", "kind" },
+        format = function(_, item)
+          local icons = require "icons.lspkind"
+          local icon = icons[item.kind] or ""
+          item.kind = string.format("%s %s ", icon, trim(item.kind))
+          item.abbr = getMethodName(trim(item.abbr))
+          item.menu = truncateString(trim(item.menu), 10)
+          return item
+        end,
+      },
+      mapping = {
+        ["<S-Tab>"] = cmp.mapping(
+          function() cmp.select_prev_item { behavior = cmp.SelectBehavior.Select } end,
+          { "i", "c" }
+        ),
+        ["<Tab>"] = cmp.mapping(
+          function() cmp.select_next_item { behavior = cmp.SelectBehavior.Select } end,
+          { "i", "c" }
+        ),
+        ["<C-N>"] = cmp.mapping(function()
+          if luasnip.jumpable(1) then luasnip.jump(1) end
+        end, { "i", "c" }),
+        ["<C-P>"] = cmp.mapping(function()
+          if luasnip.jumpable(-1) then luasnip.jump(-1) end
+        end, { "i", "c" }),
+      },
+      window = {
+        completion = {
+          col_offset = 1,
+          side_padding = 1,
+          scrollbar = false,
+        },
+      },
+    })
+  end,
 }
