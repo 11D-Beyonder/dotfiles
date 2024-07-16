31158ff53a41c89f1bf8c185d535583119312ae9
Tianze Zhu <ztz20001117@gmail.com>
2024-07-16 20:43:32 +0800

    refactor(wezterm): font size.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.color_scheme = "Bamboo"
 config.font = wezterm.font("FantasqueSansM Nerd Font")
-config.font_size = 18
+config.font_size = 16
 
 local platform = require("utils.platform")()
 if platform.is_win then
