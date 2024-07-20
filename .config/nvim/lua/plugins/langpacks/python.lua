4a7a4b251888a42d701ac73ed4871c2823fbe9a4
11D-Beyonder <ztz20001117@126.com>
2024-07-21 02:56:37 +0800

    feat(nvim): update config for python -- https://github.com/chaozwn/astronvim_with_coc_or_mason/commit/a9ba2729632ecc26b571cd55aec4a1248fe72fa4

--- .config/nvim/lua/plugins/langpacks/python.lua
+++ .config/nvim/lua/plugins/langpacks/python.lua
@@ -1,30 +1,60 @@
+local utils = require "astrocore"
+local is_available = require("astrocore").is_available
+local set_mappings = require("astrocore").set_mappings
+
 return {
   {
     "AstroNvim/astrolsp",
-    optional = true,
     opts = {
       config = {
         basedpyright = {
-          before_init = function(_, c)
-            if not c.settings then c.settings = {} end
-            if not c.settings.python then c.settings.python = {} end
-            c.settings.python.pythonPath = vim.fn.exepath "python"
+          on_attach = function()
+            if is_available "venv-selector.nvim" then
+              set_mappings({
+                n = {
+                  ["<Leader>lv"] = {
+                    "<cmd>VenvSelect<CR>",
+                    desc = "Select VirtualEnv",
+                  },
+                  ["<leader>lV"] = {
+                    function()
+                      require("astrocore").notify(
+                        "Current Env:" .. require("venv-selector").get_active_venv(),
+                        vim.log.levels.INFO
+                      )
+                    end,
+                    desc = "Show Current VirtualEnv",
+                  },
+                  ["<leader>lo"] = {
+                    "<cmd>PyrightOrganizeImports<CR>",
+                    desc = "Organize Imports",
+                  },
+                },
+              }, { buffer = true })
+            end
+          end,
+          filetypes = { "python" },
+          single_file_support = true,
+          root_dir = function(...)
+            local util = require "lspconfig.util"
+            return util.find_git_ancestor(...)
+              or util.root_pattern(unpack {
+                "pyproject.toml",
+                "setup.py",
+                "setup.cfg",
+                "requirements.txt",
+                "Pipfile",
+                "pyrightconfig.json",
+              })(...)
           end,
           settings = {
             basedpyright = {
               analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                useLibraryCodeForTypes = true,
+                reportMissingTypeStubs = false,
                 typeCheckingMode = "basic",
-                autoImportCompletions = true,
-                stubPath = vim.env.HOME .. "/typings",
-                diagnosticSeverityOverrides = {
-                  reportUnusedImport = "information",
-                  reportUnusedFunction = "information",
-                  reportUnusedVariable = "information",
-                  reportGeneralTypeIssues = "none",
-                  reportOptionalMemberAccess = "none",
-                  reportOptionalSubscript = "none",
-                  reportPrivateImportUsage = "none",
-                },
               },
             },
           },
@@ -37,8 +67,25 @@ return {
     optional = true,
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python", "toml" })
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "python" })
       end
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "black", "isort" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "python" })
+      if not opts.handlers then opts.handlers = {} end
+      -- NOTE: 取消mason-nvim-dap的设置，调试功能由nvim-dap-python接管。
+      opts.handlers.python = function() end
     end,
   },
   {
@@ -49,60 +96,37 @@ return {
     end,
   },
   {
-    "jay-babu/mason-null-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "black", "isort" })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python" })
-      if not opts.handlers then opts.handlers = {} end
-      opts.handlers.python = function() end
-    end,
-  },
-  {
     "linux-cultist/venv-selector.nvim",
-    branch = "regexp",
-    dependencies = {
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          mappings = {
-            n = {
-              ["<Leader>lv"] = { "<Cmd>VenvSelect<CR>", desc = "Select VirtualEnv" },
-            },
-          },
+    opts = {
+      anaconda_base_path = "$HOME/protable/miniconda3",
+      anaconda_envs_path = "$HOME/protable/miniconda3/envs",
+      stay_on_this_version = true,
+      dap_enabled = true,
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
         },
       },
     },
-    cmd = "VenvSelect",
-    config = true,
+    cmd = { "VenvSelect", "VenvSelectCached" },
   },
   {
     "mfussenegger/nvim-dap-python",
     dependencies = "mfussenegger/nvim-dap",
     ft = "python",
     config = function(_, opts)
-      local path = vim.fn.exepath "python"
-      local debugpy = require("mason-registry").get_package "debugpy"
-      if debugpy:is_installed() then
-        path = debugpy:get_install_path()
-        if vim.fn.has "win32" == 1 then
-          path = path .. "/venv/Scripts/python"
-        else
-          path = path .. "/venv/bin/python"
-        end
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      if vim.fn.has "win32" == 1 then
+        path = path .. "/venv/Scripts/python"
+      else
+        path = path .. "/venv/bin/python"
       end
       require("dap-python").setup(path, opts)
     end,
   },
   {
     "nvim-neotest/neotest",
+    optional = true,
     dependencies = { "nvim-neotest/neotest-python" },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
