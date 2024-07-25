d6669d13bd13f3bc10d37f90840e5c3d0dd4d10b
Tianze Zhu <ztz20001117@gmail.com>
2024-07-25 16:16:19 +0800

    refactor(wezterm): remove utils.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,9 +3,4 @@ local config = wezterm.config_builder()
 config.color_scheme = "Bamboo"
 config.font = wezterm.font("FantasqueSansM Nerd Font")
 config.font_size = 16
-
-local platform = require("utils.platform")()
-if platform.is_win then
-	config.default_prog = { "pwsh" }
-end
 return config
