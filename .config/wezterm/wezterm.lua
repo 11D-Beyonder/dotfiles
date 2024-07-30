d3bd6d9d02af0c016f0acd6a1b8f31d62a62b5f2
Tianze Zhu <ztz20001117@gmail.com>
2024-07-30 16:10:19 +0800

    refactor(wezterm): return config table.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-config.color_scheme = "Bamboo"
-config.font = wezterm.font("FantasqueSansM Nerd Font")
-config.font_size = 16
+local config = {
+	color_scheme = "Bamboo",
+
+	font = wezterm.font("FantasqueSansM Nerd Font"),
+	font_size = 16,
+}
 return config
