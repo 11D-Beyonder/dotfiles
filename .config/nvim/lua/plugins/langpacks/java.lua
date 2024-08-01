48b0e14a8f09c282aa8290d7ae0ea325d810a4f6
Tianze Zhu <ztz20001117@gmail.com>
2024-08-01 22:15:32 +0800

    feat(nvim): add neotest-java.

--- .config/nvim/lua/plugins/langpacks/java.lua
+++ .config/nvim/lua/plugins/langpacks/java.lua
@@ -126,6 +126,19 @@ return {
     end,
   },
   {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "rcasia/neotest-java",
+      ft = "java",
+      cmd = "NeotestJava",
+    },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-java"(require("astrocore").plugin_opts "neotest-java"))
+    end,
+  },
+  {
     "eatgrass/maven.nvim",
     cmd = { "Maven", "MavenExec" },
     dependencies = {
