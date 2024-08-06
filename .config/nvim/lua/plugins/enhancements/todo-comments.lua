eeb4fcbd4325d507a567188550788120ad73936b
Tianze Zhu <ztz20001117@gmail.com>
2024-08-06 19:18:20 +0800

    feat(nvim): add QUES keywords in todo-comments.

---
+++ .config/nvim/lua/plugins/enhancements/todo-comments.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/todo-comments.nvim",
+  opts = {
+    keywords = {
+      QUES = { icon = "󱍊", color = "info" },
+    },
+    colors = {
+      question = { "Question", "#FFD700" },
+    },
+  },
+}
