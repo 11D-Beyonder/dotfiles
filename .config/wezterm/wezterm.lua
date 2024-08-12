1cc5943b14d1f93443cb371ba09911550e2223de
Tianze Zhu <ztz20001117@gmail.com>
2024-08-12 21:33:22 +0800

    feat(wezterm): change font to CaskaydiaCove Nerd Font.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {
 	color_scheme = "Bamboo",
 
-	font = wezterm.font("FantasqueSansM Nerd Font"),
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
 	font_size = 16,
 }
 return config
