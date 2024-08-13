7d8f849ffcf858429226ab511126291908440877
Tianze Zhu <ztz20001117@gmail.com>
2024-08-13 19:43:04 +0800

    feat(wezterm): change font to maple.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {
 	color_scheme = "Bamboo",
 
-	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	font = wezterm.font("Maple Mono"),
 	font_size = 16,
 }
 return config
