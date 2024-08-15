8700071f4cbe480a3dfff350ba2505b0fcef8fd5
11D-Beyonder <ztz20001117@126.com>
2024-08-15 13:09:12 +0800

    feat(nvim): remove sqls.

--- .config/nvim/lua/plugins/langpacks/sql.lua
+++ .config/nvim/lua/plugins/langpacks/sql.lua
@@ -8,32 +8,4 @@ return {
       end
     end,
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "sqls" })
-    end,
-  },
-  {
-    "nanotee/sqls.nvim",
-    lazy = true,
-    dependencies = {
-      "AstroNvim/astrocore",
-      opts = {
-        autocmds = {
-          sqls_attach = {
-            {
-              event = "LspAttach",
-              desc = "Load sqls.nvim with sqls",
-              callback = function(args)
-                local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-                if client.name == "sqls" then require("sqls").on_attach(client, args.buf) end
-              end,
-            },
-          },
-        },
-      },
-    },
-  },
 }
