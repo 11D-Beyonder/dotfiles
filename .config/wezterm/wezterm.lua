f3874e290fe3c6f4c800c5e6faf69de2464be8a5
11D-Beyonder <ztz20001117@126.com>
2024-08-25 22:17:34 +0800

    feat(wezterm): change font to 'Maple Mono NF CN'.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {
 	color_scheme = "Bamboo",
 
-	font = wezterm.font("Maple Mono"),
+	font = wezterm.font("Maple Mono NF CN"),
 	font_size = 16,
 }
 return config
