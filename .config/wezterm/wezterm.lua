7150dbb247e432db396328225c39b6c95688f948
11D-Beyonder <ztz20001117@126.com>
2024-09-04 13:51:15 +0800

    feat(wezterm): auto dark mode.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,21 @@
 local wezterm = require("wezterm")
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Bamboo"
+	else
+		return "Bamboo Light"
+	end
+end
+
 local config = {
-	color_scheme = "Bamboo",
+	color_scheme = scheme_for_appearance(get_appearance()),
 
 	font = wezterm.font("Maple Mono NF CN"),
 	font_size = 16,
--- .zshrc
+++ .zshrc
@@ -126,11 +126,6 @@ alias unproxy="unset https_proxy; unset http_proxy; unset all_proxy"
 alias ff="fastfetch"
 alias zj="zellij"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export NVM_NODEJS_ORG_MIRROR="https://mirrors.ustc.edu.cn/node/"
-
 
 export EDITOR="nvim"
 
