44e09674b33cd66a309147221efe3700ec1d6441
tianze.zhu <tianze.zhu@MMC91F49TH.local>
2025-08-08 11:57:37 +0800

    feat(nvim): add toggleterm keymaps.

--- .config/nvim/lua/plugins/core/fzf.lua
+++ .config/nvim/lua/plugins/core/fzf.lua
@@ -5,13 +5,14 @@ return {
 	cond = not vim.g.vscode,
 	opts = {},
 	keys = {
-		{ "<leader>ff", "<cmd>FzfLua files<cr>", mode = "n", desc = "[F]zfLua: Find [F]iles" },
-		{ "<leader>fh", "<cmd>FzfLua helptags<cr>", mode = "n", desc = "[F]zfLua: Find Helptags" },
-		{ "<leader>fg", "<cmd>FzfLua live_grep_native<cr>", mode = "n", desc = "[F]zfLua: Live [G]rep in CWD" },
-		{ "<leader>f/", "<cmd>FzfLua lgrep_curbuf<cr>", mode = "n", desc = "[F]zfLua: Live [G]rep in Current Buffer" },
+		{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "[F]zfLua: Find [F]iles" },
+		{ "<leader>fh", "<cmd>FzfLua helptags<cr>", desc = "[F]zfLua: Find Helptags" },
+		{ "<leader>fg", "<cmd>FzfLua live_grep_native<cr>", desc = "[F]zfLua: Live [G]rep in CWD" },
+		{ "<leader>f/", "<cmd>FzfLua lgrep_curbuf<cr>", desc = "[F]zfLua: Live [G]rep in Current Buffer" },
 		{ "<leader>fv", "<cmd>FzfLua grep_visual<cr>", mode = "v", desc = "[F]zfLua: Grep [V]isual" },
-		{ "<leader>fn", "<cmd>NoiceFzf<cr>", mode = "n", desc = "[F]zfLua: Find [N]otifications" },
-		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", mode = "n", desc = "[F]zfLua: Find [B]uffers" },
+		{ "<leader>fn", "<cmd>NoiceFzf<cr>", desc = "[F]zfLua: Find [N]otifications" },
+		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "[F]zfLua: Find [B]uffers" },
+		{ "<leader>ft", "<cmd>TermSelect<cr>", desc = "[F]zfLua: Find [T]erminals" },
 	},
 	config = function(_, opts)
 		require("fzf-lua").setup(opts)
--- .config/nvim/lua/plugins/ui/toggleterm.lua
+++ .config/nvim/lua/plugins/ui/toggleterm.lua
@@ -1,7 +1,11 @@
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new { cmd = "lazygit", hidden = true, direction = "float" }
+
+function _lazygit_toggle() lazygit:toggle() end
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-
+	cmd = { "Toggleterm", "TermSelect" },
 	opts = {
 		size = function(term)
 			if term.direction == "horizontal" then
@@ -11,5 +15,8 @@ return {
 			end
 		end,
 		open_mapping = [[<c-\>]],
+	},
+	keys = {
+		{ "<leader>tg", "<cmd>lua _lazygit_toggle()<cr>", desc = "Toggle Lazygit" },
 	},
 }
